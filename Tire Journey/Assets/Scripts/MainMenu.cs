1ada80e3cba3c747955e3d59fa286a0a512b3184
Tanner Townsend <rsmcwow@gmail.com>
2022-03-09 22:36:40 -0500

    Added menu screen with custom buttons

---
+++ Tire Journey/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
